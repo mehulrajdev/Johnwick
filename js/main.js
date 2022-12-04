db2bb456b7229d7289c8bc17eb7be1ea1f5b33b6
Mehul Raj <82897404+mehulrajdev@users.noreply.github.com>
2022-12-04 19:47:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 var btn = $('.btn');
 var box = $('.box');
 
-
-
 btn.on('click', function () {
   box.fadeToggle(2000);
 })
-
-
 
 var btnAchievements = $('.btn-achievements');
 var boxAchievements = $('.box-achievements');
