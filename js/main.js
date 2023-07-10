87f3979c07d7803ad7eeaf9116773d0ca3875984
mehulrajdev <mehulrajpnbe@gmail.com>
2023-07-10 17:22:39 +0530

    edited style.css

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
-var btn = $('.btn');
-var box = $('.box');
 
-btn.on('click', function () {
-  box.fadeToggle(2000);
-})
-
-
-var btnAchievements = $('.btn-achievements');
-var boxAchievements = $('.box-achievements');
-
-
-btnAchievements.on('click', function () {
-  boxAchievements.fadeToggle(2000);
-})
-
-var btnAchievements2 = $('.btn-achievements2');
-var boxAchievements2 = $('.box-achievements2');
-
-btnAchievements2.on('click', function () {
-  boxAchievements2.fadeToggle(2000);
-})
-
-var btnAchievements3 = $('.btn-achievements3');
-var boxAchievements3 = $('.box-achievements3');
-
- btnAchievements3.on('click', function () {
-  boxAchievements3.fadeToggle(2000);
- })
-
-var btncontacts = $('.btn-contacts');
-var boxcontacts = $('.box-contacts');
-
-
- 
 btncontacts.on('click', function () {
-  boxcontacts.fadeToggle(1000);
+  boxcontacts.fadeToggle(2000);
 })
-
-
